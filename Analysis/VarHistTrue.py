1397cb881dcb0233bfe82e8f086c820504bbd254
Diogo Pereira <diogo1barros1pereira@gmail.com>
2024-07-23 09:05:19 +0100

    Remove commented code lines regarding data analysis.

--- Analysis/VarHistTrue.py
+++ Analysis/VarHistTrue.py
@@ -15,32 +15,21 @@ from ROOT import TFile, TH1F, TCanvas, TLegend
 dir = "~/Data/" # Diogo
 #dir = "/user/u/u24gmarujo/root_folder/" #Gonçalo
 data_file1 = "B0ToKstMuMu_JpsiMC_22F_0-1531_miniaod.root"
-#data_file2 = "B0ToKstMuMu_22F_0-812_miniaod.root"
 
 # Open .root files
 root_file1 = TFile.Open(dir + data_file1)
-#root_file2 = TFile.Open(dir + data_file2)
 
 if not root_file1 or root_file1.IsZombie():
     print(f"Error: Could not open file {dir + data_file1}")
     exit()
-    
-#if not root_file2 or root_file2.IsZombie():
-#    print(f"Error: Could not open file {dir + data_file2}")
-#    exit()
 
 tree1 = root_file1.Get("B0KstMuMu/B0KstMuMuNTuple")
-#tree2 = root_file2.Get("B0KstMuMu/B0KstMuMuNTuple")
 
 if not tree1:
     print("Error: Could not find tree 'B0KstMuMuNTuple' in the first file.")
     exit()
     
-#if not tree2:
-#    print("Error: Could not find tree 'B0KstMuMuNTuple' in the second file.")
-#    exit()
-
-# Create histograms for each file
+# Create histograms for each parameter
 hist1 = {
     "h_bTMass": TH1F("h_bTMass1", "True B Mass", 100, 4.5, 6.0),
     "h_kstTMass": TH1F("h_kstTMass1", "True K* Mass", 100, 0, 3),
@@ -53,18 +42,6 @@ hist1 = {
     "h_kstTrkmDCABSs": TH1F("h_kstTrkmDCABSs1", "Significance K* TrkmDCABS", 100, -10, 10),
 }
 
-#hist2 = {
-#    "h_bTMass": TH1F("h_bTMass2", "True B Mass", 100, 4.5, 6.0),
-#    "h_kstTMass": TH1F("h_kstTMass2", "True K* Mass", 100, 0, 3),
-#    "h_mumuMass": TH1F("h_mumuMass2", "MuMu Mass", 100, 0.8, 11),
-#    "h_bCosAlphaBS": TH1F("h_bCosAlphaBS2", "B CosAlphaBS", 100, -1.0, 1.0),
-#    "h_bVtxCL": TH1F("h_bVtxCL2", "B VtxCL", 100, 0, 1.0),
-#    "h_bLBSs": TH1F("h_bLBSs2", "Significance B LBS", 100, 0, 20),
-#    "h_bDCABSs": TH1F("h_bDCABSs2", "Significance B DCABS", 100, 0, 20),
-#    "h_kstTrkpDCABSs": TH1F("h_kstTrkpDCABSs2", "Significance K* TrkpDCABS", 100, 0, 10),
-#    "h_kstTrkmDCABSs": TH1F("h_kstTrkmDCABSs2", "Significance K* TrkmDCABS", 100, 0, 10),
-#}
-
 # Function to fill histograms from a tree
 def fill_hist(tree, histograms):
 
@@ -73,7 +50,6 @@ def fill_hist(tree, histograms):
 
         truthMatchSignal = getattr(tree, "truthMatchSignal")
         
-#        print(f"Entry {i}:")
 #        for idx, t_match in enumerate(truthMatchSignal):
 #            print(f"  truthMatchSignal[{idx}] = {bool(t_match)}")
 
@@ -98,8 +74,6 @@ def fill_hist(tree, histograms):
 
             if t_match or True:
 
-#                print(f"GenSignal in event {i}:{genSignal}.\n")
-
                 if genSignal == 1:
                     histograms["h_bTMass"].Fill(bMass[idx])
                     histograms["h_kstTMass"].Fill(kstMass[idx])
@@ -108,7 +82,6 @@ def fill_hist(tree, histograms):
                     histograms["h_kstTMass"].Fill(kstBarMass[idx])
 
                 histograms["h_mumuMass"].Fill(mumuMass[idx])
-#                print(f"Value of mumu Mass that we are filling the Hist with: {mumuMass[idx]}.\n")
                 histograms["h_bCosAlphaBS"].Fill(bCosAlphaBS[idx])
                 histograms["h_bVtxCL"].Fill(bVtxCL[idx])
 
@@ -126,66 +99,19 @@ def fill_hist(tree, histograms):
 
 # Fill histograms for both trees
 fill_hist(tree1, hist1)
-#fill_hist(tree2, hist2)
-
-# Normalize the Histograms
-#for key, hist in hist1.items():
-#    hist.Scale(1./hist.GetMaximum())
- 
-#for key, hist in hist2.items():
-#    hist.Scale(1./hist.GetMaximum())
            
-# Function to plot two histograms on the same canvas and save as .pdf
+# Function to plot a histogram and save as .pdf
 def plot_hist(hist1, file_name, title):
+
     canvas = ROOT.TCanvas("canvas", "Canvas", 800, 600)
     
-    # Set line colors for histograms
-#    hist1.SetLineColor(ROOT.kRed)
-#    hist2.SetLineColor(ROOT.kBlue)
-    
-    # Set titles for histograms
     hist1.SetTitle(title)
-#    hist2.SetTitle(title)
-    
-    # Draw histograms on the main canvas
-    hist1.Draw("HIST")
-#    hist2.Draw("HIST SAME")
 
-#    hist1.SetStats(0)
-#    hist2.SetStats(0)
-    
-    # Calculate statistics for hist1
-#    entries1 = hist1.GetEntries()
-#    mean1 = hist1.GetMean()
-#    stddev1 = hist1.GetStdDev()
-    
-    # Calculate statistics for hist2
-#    entries2 = hist2.GetEntries()
-#    mean2 = hist2.GetMean()
-#    stddev2 = hist2.GetStdDev()
-    
-    # Create custom statistics box for hist1
-#    stats1 = ROOT.TPaveText(0.6, 0.9, 0.8, 1.0, "NDC")
-#    stats1.SetBorderSize(1)
-#    stats1.SetTextColor(ROOT.kRed)
-#    stats1.AddText(f"Monte Carlo: Entries = {entries1:.0f}")
-#    stats1.AddText(f"Mean = {mean1:.3f}")
-#    stats1.AddText(f"Std Dev = {stddev1:.3f}")
-#    stats1.Draw()
-    
-    # Create custom statistics box for hist2
-#    stats2 = ROOT.TPaveText(0.8, 0.9, 1.0, 1.0, "NDC")
-#    stats2.SetBorderSize(1)
-#    stats2.SetTextColor(ROOT.kBlue)
-#    stats2.AddText(f"Data: Entries = {entries2:.0f}")
-#    stats2.AddText(f"Mean = {mean2:.3f}")
-#    stats2.AddText(f"Std Dev = {stddev2:.3f}")
-#    stats2.Draw()
-    
-    # Create a legend and add entries with statistics
+    hist1.Draw("HIST")
+
+    # Create a legend
     legend = ROOT.TLegend(0.8, 0.2, 1.0, 0.3)  # Adjusted to not overlap with stats boxes
     legend.AddEntry(hist1, "Monte Carlo", "l")
-#    legend.AddEntry(hist2, "Data", "l")
     legend.Draw()
     
     # Update the main canvas to reflect changes
@@ -212,11 +138,7 @@ output_file = TFile("VariabHist3.root", "RECREATE")
 for key, hist in hist1.items():
     hist.Write()
       
-#for key, hist in hist2.items():
-#    hist.Write()
-  
 output_file.Close()
   
-# Close .root files
+# Close .root file
 root_file1.Close()
-#root_file2.Close()
