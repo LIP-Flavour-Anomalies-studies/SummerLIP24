05ea52f5863282434f218cab76af6a0948ddf866
Diogo Pereira <diogo1barros1pereira@gmail.com>
2024-08-25 11:46:05 +0100

    Alpha and Focal loss comparison with Optuna optimization.

---
+++ Machine_Learning/Diogo/Evaluation/Optuna_FocalLoss/Evaluation.py
@@ -0,0 +1,259 @@
+"""
+Created on August 2024
+
+@author: Diogo Pereira
+Gonçalo Marujo
+
+LIP Internship Program | Flavour Anomalies
+"""
+
+
+import torch
+import torch.nn as nn
+import sys
+import os
+from fpdf import FPDF
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
+
+# Add the directory containing NN.py to the Python path
+sys.path.append(os.path.abspath('/user/u/u24diogobpereira/LocalRep/Machine_Learning/Diogo/Training/Optuna_FocalLoss/'))
+sys.path.append(os.path.abspath('/user/u/u24diogobpereira/LocalRep/Machine_Learning/Diogo/Training/'))
+
+from NN import ClassificationModel 
+from sklearn.metrics import roc_curve, auc, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+
+
+def load_model():
+    # Load checkpoint
+    checkpoint = torch.load('model_checkpoint.pth')
+
+    # Load test dataset from checkpoint
+    dataset = checkpoint['dataset']
+    test_dataset = checkpoint['test_set']
+
+    # Create DataLoader for test data
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    
+    a_lr = checkpoint['alpha_hyperparameters']["lr"]
+    a_n_layers = checkpoint['alpha_hyperparameters']["n_layers"]
+    a_n_units = [checkpoint['alpha_hyperparameters'][f"n_units_l{i}"] for i in range(a_n_layers)]
+    activation_functions = {'ReLU': nn.ReLU, 'Tanh': nn.Tanh, 'LeakyReLU': nn.LeakyReLU}
+    a_activation = activation_functions[checkpoint['alpha_hyperparameters']["activation"]]
+    
+    f_lr = checkpoint['focal_hyperparameters']["lr"]
+    f_n_layers = checkpoint['focal_hyperparameters']["n_layers"]
+    f_n_units = [checkpoint['focal_hyperparameters'][f"n_units_l{i}"] for i in range(f_n_layers)]
+    f_activation = activation_functions[checkpoint['focal_hyperparameters']["activation"]]
+    
+    # Recreate the model and load state dict
+    input_size = test_dataset.dataset.X.shape[1]
+    alpha_model = ClassificationModel(input_size, a_n_layers, a_n_units, a_activation)
+    focal_model = ClassificationModel(input_size, f_n_layers, f_n_units, f_activation)
+    alpha_model.load_state_dict(checkpoint['alpha_model_state_dict'])
+    focal_model.load_state_dict(checkpoint['focal_model_state_dict'])
+   
+    
+    return alpha_model, focal_model, test_loader
+
+
+
+
+def get_targets_probabilities(model, test_loader):
+    targets = []
+    probabilities = []
+
+    # Loop through dataloader minimizes memory usage
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs).squeeze()
+            probabilities.extend(outputs.cpu().numpy())  # Save the probability for ROC curve
+            targets.extend(labels.cpu().numpy())
+        
+    return targets, probabilities
+    
+    
+    
+    
+   
+def get_predictions(model, test_loader, best_thresh):
+    predictions = []
+    
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs).squeeze()
+            predicted_labels = (outputs >= best_thresh).float()
+            predictions.extend(predicted_labels.cpu().numpy())
+            
+    return predictions
+   
+   
+
+
+
+def plot_histograms(targets, probabilities, best_thresh, name):
+    prob = np.array(probabilities)
+    targets = np.array(targets)
+
+    plt.figure(figsize=(8, 6))
+
+    # Background predictions
+    background_predict = prob[targets == 0]
+    plt.hist(background_predict, bins=40, range=(0,1), density=True, alpha=0.5, label="Background (ED)", color="red", hatch="//", edgecolor="black")
+    
+    # Signal predictions
+    signal_predict = prob[targets == 1]
+    plt.hist(signal_predict, bins=40, range=(0,1), density=True, alpha=0.7, label="Signal (MC)", color="blue")
+    
+    plt.axvline(x=best_thresh, color='grey', lw=2, label=f'Threshold = {best_thresh:.2f}')
+    plt.xlabel("Predicted Probability", fontsize=14, labelpad=15)
+    plt.ylabel("Normalized Density", fontsize=14, labelpad=15) 
+    plt.legend()
+    plt.savefig(f"{name}_prob_dsitribution.pdf")  # Save the plot as a PDF file
+    plt.close()
+
+
+
+
+
+def scatter_plots(test_loader, probabilities, targets, filename):
+
+    # Convert probabilities to a numpy array
+    prob = np.array(probabilities)
+    targets = np.array(targets)
+    
+    # Split probabilities based on target labels
+    signal_predict = prob[targets == 1]
+    background_predict = prob[targets == 0]
+    
+    # Define feature columns
+    columns = ["kstTMass", "bCosAlphaBS", "bVtxCL", "bLBSs", "bDCABSs", "kstTrkpDCABSs", "kstTrkmDCABSs", "leadingPt", "trailingPt"]
+
+    for i in range(len(columns)):
+        variables = []
+        with torch.no_grad():
+            name = columns[i]
+
+            # Collect the feature values for the i-th column
+            for inputs, labels in test_loader:
+                variables.extend(inputs[:, i].cpu().numpy())
+                   
+            variables = np.array(variables)
+           
+        # Split feature values based on target label
+        signal_variable = variables[targets == 1]
+        background_variable = variables[targets == 0]
+           
+   
+        # Create scatter plot
+        plt.figure(figsize=(8, 6))
+        plt.scatter(signal_variable, signal_predict, marker='o', color='blue', label="Signal (MC)")
+        plt.scatter(background_variable, background_predict, marker='o', color='red', alpha = 0.5, label="Background (DATA)")    
+        plt.xlabel(name, fontsize=14, labelpad=15)
+        plt.ylabel("Predicted Probability", fontsize=14, labelpad=15) 
+        plt.legend()
+        plt.savefig(f"{filename}_{name}_scatter_plot.pdf")  # Save the plot as a PDF file
+        plt.close()
+ 
+ 
+
+
+def save_metrics(accuracy, precision, recall, f1, c, name):
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    # Add accuracy, precision, recall, and F1 score
+    pdf.cell(200, 10, txt=f"Accuracy -> Fraction of correctly classified samples:\n {accuracy:.4f}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"Precision -> Ability of the classifier not to label as signal a sample that is background:\n {precision:.4f}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"Recall -> Ability of classifier to find all signal samples:\n {recall:.4f}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"F1 score -> Harmonic mean of precision and recall:\n {f1:.4f}\n", ln=True, align='L')
+
+    # Add confusion matrix
+    pdf.cell(200, 10, txt="Confusion Matrix:", ln=True, align='L')
+    pdf.cell(200, 10, txt="|tn fp|", ln=True, align='L')
+    pdf.cell(200, 10, txt="|fn tp|\n", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"|{c[0,0]} {c[0,1]}|", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"|{c[1,0]} {c[1,1]}|\n", ln=True, align='L')
+    
+    pdf.output(f"{name}_classification_report.pdf")
+    
+
+
+
+'''
+-------------------------------------MAIN--------------------------------------------
+'''
+
+
+
+
+alpha_model, focal_model, test_loader = load_model()       
+alpha_model.eval()  # Set model to evaluation mode
+focal_model.eval()
+
+models = {
+    "alpha_model": alpha_model,
+    "focal_model": focal_model
+}
+
+# Plot ROC curve
+plt.figure()
+plt.plot([0, 1], [0, 1], color='darkgreen', lw=2, linestyle='--', label = 'Random Classifier')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+
+for model_name, model in models.items():
+
+    # Compute AUC
+    targets, probabilities = get_targets_probabilities(model, test_loader)    
+    fpr, tpr, thresholds = roc_curve(targets, probabilities)
+    roc_auc = auc(fpr, tpr)
+       
+    # Get the best threshold
+    '''
+    J = tpr - 1.5 * fpr
+    ix = np.argmax(J)
+    best_thresh = thresholds[ix]
+    print('\nBest Threshold = %f' % (best_thresh))
+    '''
+    
+    f1_vector=[]
+    for thresh in thresholds:
+        predictions = get_predictions(model, test_loader, thresh)
+        f1_vector.append(f1_score(targets, predictions))
+    
+    f1_vector = np.array(f1_vector)
+    ix = np.argmax(f1_vector)
+    best_thresh = thresholds[ix]
+
+    # Get predictions 
+    predictions = get_predictions(model, test_loader, best_thresh)
+
+    # Compute metrics with sklearn   
+    accuracy = accuracy_score(targets, predictions)
+    precision = precision_score(targets, predictions)
+    recall = recall_score(targets, predictions) # (tp / (tp + fn)) = tpr[best_thresh] 
+    f1 = f1_score(targets, predictions)
+    c = confusion_matrix(targets, predictions)
+    
+    if model_name == "alpha_model":
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'Alpha Loss (area = {roc_auc:.4f})')
+        plt.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Alpha cut')
+    
+    if model_name == "focal_model":
+        plt.plot(fpr, tpr, color='navy', lw=2, alpha = 0.5, label=f'Focal Loss (area = {roc_auc:.4f})')
+        plt.scatter(fpr[ix], tpr[ix], marker='o', color='red', label='Focal cut')
+   
+    save_metrics(accuracy, precision, recall, f1, c, model_name)   
+    plot_histograms(targets, probabilities, best_thresh, model_name)
+    scatter_plots(test_loader, probabilities, targets, model_name)
+
+# Save the plot as a PDF file
+plt.legend(loc='lower right')
+plt.savefig(f"roc_curve.pdf")
+plt.close()  
--- Machine_Learning/Diogo/Training/Optuna_FocalLoss/NN.py
+++ Machine_Learning/Diogo/Training/Optuna_FocalLoss/NN.py
@@ -12,7 +12,6 @@ import uproot3 as uproot
 import awkward0 as ak
 import numpy as np
 import os
-import prepdata as prep
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -47,18 +46,44 @@ class ClassificationModel(nn.Module):
    
    
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2):
+    def __init__(self, alpha=None, gamma=2):
         super(FocalLoss, self).__init__()
-        self.alpha = alpha
+        self.alpha = alpha  
         self.gamma = gamma
 
     def forward(self, inputs, targets):
         BCE_loss = nn.functional.binary_cross_entropy(inputs, targets, reduction="none")
         pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
-        return torch.mean(F_loss)   
-   
-   
+
+        if self.alpha is not None:
+            alpha_t = self.alpha[1] * targets + self.alpha[0] * (1 - targets)
+            F_loss = alpha_t * (1-pt)**self.gamma * BCE_loss
+        else:
+            F_loss = (1-pt)**self.gamma * BCE_loss
+
+        return torch.mean(F_loss)
+
+
+
+
+
+class AlphaLoss(nn.Module):
+    def __init__(self, alpha=None):
+        super(AlphaLoss, self).__init__()
+        self.alpha = alpha  
+
+    def forward(self, inputs, targets):
+        BCE_loss = nn.functional.binary_cross_entropy(inputs, targets, reduction="none")
+
+        if self.alpha is not None:
+            alpha_t = self.alpha[1] * targets + self.alpha[0] * (1 - targets)
+            F_loss = alpha_t * BCE_loss
+        else:
+            F_loss = BCE_loss
+
+        return torch.mean(F_loss)
+
+
    
    
       
@@ -71,6 +96,7 @@ class EarlyStopping:
         self.best_score = None
         self.early_stop = False
         self.counter = 0
+        self.stability = 0
         self.best_model_state = None
 
     def __call__(self, val_loss, model):
@@ -79,7 +105,7 @@ class EarlyStopping:
         if self.best_score is None:
             self.best_score = score
             self.best_model_state = model.state_dict()
-        elif score < self.best_score - self.delta:
+        elif score < self.best_score - self.delta and self.best_score - score < self.stability:
             self.counter += 1
             if self.counter >= self.patience:
                 self.early_stop = True
@@ -96,198 +122,6 @@ class EarlyStopping:
   
   
   
-def regularisation(val_loader, model, criterion, epoch, num_epochs, early_stopping):  
-  
-    model.eval()
-    v_loss = 0.0
-    
-    # Compute validation loss for 1 epoch
-    with torch.no_grad():
-        for val_inputs, val_targets in val_loader:
-            val_outputs = model(val_inputs).squeeze()
-            val_loss = criterion(val_outputs, val_targets) 
-            v_loss += val_loss.item() * val_inputs.size(0)          
-        v_loss /= len(val_loader.dataset)              
-        print(f"Epoch {epoch+1}/{num_epochs}, v_loss: {v_loss}")  
-        
-        # Check if validation loss has reached its minimum
-        early_stopping(v_loss, model)
-    
-    return v_loss
-  
-  
-  
-      
-        
-'''        
--------------------------------------------||-------------------------------------------        
-'''       
-        
 
-def train_model(model, early_stopping, train_loader, val_loader, criterion, optimizer, num_epochs=100):
-
-    stop_flag = 0
-    train_losses = []
-    validation_losses = []
-    idx = num_epochs - 1
-
-    for epoch in range(num_epochs):
-        model.train() # Switch model to training mode
-        t_loss = 0.0
-        
-        for inputs, targets in train_loader:
-            optimizer.zero_grad()                # Zero gradients to avoid accumulation between batches
-            outputs = model(inputs).squeeze() 
-            loss = criterion(outputs, targets)   # Average loss for the current batch 
-            loss.backward()                      # Computes accumulated loss gradient
-            optimizer.step()                     # Updates parameters in the direction that reduces the loss
-            t_loss += loss.item() * inputs.size(0)
-            
-        t_loss /= len(train_loader.dataset)            
-        # print(f"Epoch {epoch+1}/{num_epochs}, Loss: {t_loss}")       
-        
-        v_loss = regularisation(val_loader, model, criterion, epoch, num_epochs, early_stopping)
-        
-        # Save losses in vector for loss over epochs plot
-        train_losses.append(t_loss)   
-        validation_losses.append(v_loss) 
-        
-        # Save best epoch number
-        if early_stopping.early_stop and stop_flag == 0:
-            idx = epoch - early_stopping.patience
-            print(f"Early stopping at epoch {idx}\n Lowest loss: {-early_stopping.best_score}")
-            stop_flag = 1
-        
-    # Load the best model
-    early_stopping.load_best_model(model)
-        
-    return train_losses, validation_losses, idx
-
-
-def loss_plot(num_epochs, train_losses, validation_losses, idx):
-
-    indices = range(100, num_epochs + 1) 
-    train_losses = train_losses[99:]   
-    validation_losses = validation_losses[99:]
-        
-    # Plot the training loss
-    plt.figure()
-    plt.plot(indices[::2], train_losses[::2], marker='o', color='navy', label='Training', markersize=1)
-    plt.plot(indices[::2], validation_losses[::2], marker='o', color='darkorange', label='Validation', markersize=1)
-    plt.scatter(idx + 1, validation_losses[idx-100], marker='o', color='black', label='Early Stop', s=64)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Loss Over Epochs')
-    plt.legend()
-    plt.savefig('training_validation_loss.pdf')  
-    plt.close()  
-
-
-def objective(trial, train_loader, val_loader, input_size, class_wght, models):
-
-    # Define hyperparameters to be optimized
-    lr = trial.suggest_float("lr", 1e-6, 1e-3, log=True)
-    n_layers = trial.suggest_int("n_layers", 1, 5)
-    n_units = [trial.suggest_int(f"n_units_l{i}", 4, 128) for i in range(n_layers)]
-    activation_functions = {'ReLU': nn.ReLU, 'Tanh': nn.Tanh, 'LeakyReLU': nn.LeakyReLU}
-    activation_name = trial.suggest_categorical("activation", ['ReLU', 'Tanh', 'LeakyReLU'])
-    activation = activation_functions[activation_name]
-
-    model = ClassificationModel(input_size, n_layers, n_units, activation)
-
-    criterion = FocalLoss(alpha=class_wght[1])
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-    early_stopping = EarlyStopping(patience=200, delta=0)
-
-    _, vl_vector, idx = train_model(model, early_stopping, train_loader, val_loader, criterion, optimizer, num_epochs=1000)
-    
-    models.append(early_stopping.best_model_state)
-
-    return vl_vector[idx]
-
-
-
-
-
-
-
-'''
------------------------------------------MAIN--------------------------------------------------
-'''
-
-
-
-def main():
-    try:
-        # Input path
-        dir = "/user/u/u24diogobpereira/Data/" #Diogo
-        MC_file = "MC.root"
-        ED_file = "ED.root"
-        
-        # Prepare data
-        x, y, branches = prep.prepdata(dir, MC_file, ED_file)
-        
-        # Create dataset
-        dataset = prep.ClassificationDataset(x, y)
-
-        # Calculate lengths based on dataset size
-        total_length = len(dataset)
-        train_length = int(0.5 * total_length)
-        test_length = int(0.25 * total_length)
-        val_length = total_length - train_length - test_length
-
-        # Create random splits
-        train_set, test_set, val_set = random_split(dataset, [train_length, test_length, val_length])
-
-        # Create DataLoader for training and validation
-        train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
-        val_dataloader = DataLoader(val_set, batch_size=32, shuffle=True)
-
-        # Compute input size and class weights
-        input_size = x.shape[1]
-        total_sampl = len(y)
-        class_wght = torch.tensor([total_sampl / (2 * np.sum(y == 0)), total_sampl / (2 * np.sum(y == 1))], dtype=torch.float32)
-        
-        # Create a hyperparameter optimization study
-        models=[]
-        study = optuna.create_study(direction="minimize")
-        study.optimize(lambda trial: objective(trial, train_dataloader, val_dataloader, input_size, class_wght, models), n_trials=10)
-        
-        print("Best trial:")
-        trial = study.best_trial
-
-        print(f"\nValue: {trial.value}")
-        print("Param.: ")
-        for key, value in trial.params.items():
-            print(f"{key}: {value}")
-
-        
-        # Define the directory and filename
-        checkpoint_dir = '/user/u/u24diogobpereira/LocalRep/Machine_Learning/Diogo/Evaluation/' #Diogo
-        checkpoint_file = 'model_checkpoint.pth'
-        checkpoint_path = os.path.join(checkpoint_dir, checkpoint_file)
-
-        # Create the directory if it doesn't exist
-        os.makedirs(checkpoint_dir, exist_ok=True)
-
-        # Save model and optimizer state dict and test dataset
-        torch.save({'model_state_dict': models[trial.number],
-                    'dataset': dataset,
-                    'test_set': test_set,
-                    'hyperparameters': trial.params}, checkpoint_path)        
-       
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        
-        
-        
-        
-        
-# Ensure main runs only when the script is executed directly and not when it is imported    
-if __name__ == '__main__':
-    main()
-    
-    
     
     
